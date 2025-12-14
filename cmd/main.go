088c5e5e2e2f3c595e3d214a84e5e6c7468a8edc
Michael Torres <mic.ric.tor@gmail.com>
2025-12-14 08:13:22 -0800

    More explicitly avoid optimizations

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	// Test how long it takes to generate 1000 random integers
+	// Collect the sum to prevent compiler optimizing out the loop
 	start := time.Now()
 	baseI := 0
 	for i := 0; i < 1000; i++ {
@@ -17,13 +18,14 @@ func main() {
 	}
 	elapsed := time.Since(start)
 	fmt.Printf("Elapsed time using hyrumrand: %s\n", elapsed)
+	fmt.Printf("Sum using hyrumrand: %d\n", baseI)
 
 	stdStart := time.Now()
 	stdI := 0
-	// Test how long it takes to generate 1000 random integers using the standard library
 	for i := 0; i < 1000; i++ {
 		stdI += rand.Intn(1000)
 	}
 	stdElapsed := time.Since(stdStart)
 	fmt.Printf("Elapsed time using math/rand: %s\n", stdElapsed)
+	fmt.Printf("Sum using math/rand: %d\n", stdI)
 }
